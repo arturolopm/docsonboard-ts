f13ca01d784b1b5339a0ac71b602b070491ef6a2
Arturo <carturolopezm@gmail.com>
2023-10-06 09:04:20 -0500

    setup navbar and login with kinde

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn('min-h-screen font-sans antialiased grainy')}>
+        <NavBar />
         {children}
       </body>
     </html>
