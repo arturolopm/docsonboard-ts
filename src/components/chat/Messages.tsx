ba34a8dbedbbe6842817d88863f3890582a403e8
Arturo <carturolopezm@gmail.com>
2023-10-22 15:29:14 -0500

    Working chat api with openai, next, implement it on client messages

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,4 +1,11 @@
-const Messages = () => {
+import { trpc } from '@/app/_trpc/client'
+interface MessagesProps {
+  fileId: string
+}
+const Messages = ({ fileId }: MessagesProps) => {
+  const {} = trpc.getFileMessages.useInfiniteQuery({
+    fileId
+  })
   return <div></div>
 }
 
