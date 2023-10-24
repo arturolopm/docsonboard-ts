964c863c2a0e06be5403006a92075b687b52d8eb
Arturo <carturolopezm@gmail.com>
2023-10-24 07:50:46 -0500

    working main functionality on chat with optimistic updates and infinite scrolling

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -13,7 +13,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
   return (
     <div className='absolute bottom-0 left-0 w-full'>
-      <form className=' mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
+      <div className=' mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
         <div className=' relative flex h-full flex-1 items-stretch md:flex-col'>
           <div className=' relative flex flex-col w-full flex-grow p-4'>
             <div className=' relative'>
@@ -23,6 +23,8 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 maxRows={4}
                 ref={textareaRef}
                 autoFocus
+                onChange={handleInputChange}
+                value={message}
                 onKeyDown={(e) => {
                   if (e.key === 'Enter' && e.shiftKey) {
                     e.preventDefault()
@@ -37,7 +39,6 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
                 disabled={isLoading || isDisabled}
                 className=' absolute bottom-1.5 right-[8px]'
                 aria-label='send message'
-                type='submit'
                 onClick={(e) => {
                   e.preventDefault()
                   addMessage()
@@ -48,7 +49,7 @@ const ChatInput = ({ isDisabled }: ChatInputProps) => {
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   )
 }
