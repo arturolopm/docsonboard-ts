9353ddbdd11d4cb950c4c86264d6b66ed74c7d53
Arturo <carturolopezm@gmail.com>
2023-10-26 15:12:53 -0500

    Ready to deploy

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -4,6 +4,16 @@ import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { useToast } from './ui/use-toast'
 import { trpc } from '@/app/_trpc/client'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from './ui/card'
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+import { format } from 'date-fns'
 
 interface BillingFormProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
@@ -32,7 +42,35 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
         onSubmit={(e) => {
           e.preventDefault()
           createStripeSession()
-        }}></form>
+        }}>
+        {/* todo: i18n */}
+        <Card>
+          <CardHeader>
+            <CardTitle>Subscription Plan</CardTitle>
+            <CardDescription>
+              You are currently on the <strong>{subscriptionPlan.name}</strong>{' '}
+              plan.
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className=' flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0'>
+            <Button type='submit'>
+              {isLoading && <Loader2 className=' mr-4 h-4 w-4 animate-spin' />}
+              {subscriptionPlan.isSubscribed
+                ? 'Manage subscription'
+                : 'Upgrade to PRO'}
+            </Button>
+            {subscriptionPlan.isSubscribed && (
+              <p className=' rounded-full text-xs font-medium'>
+                {subscriptionPlan.isCanceled
+                  ? 'Your plan will be cancelled on '
+                  : 'Your plan renews on '}
+                {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.mm.yyyy')}
+                .
+              </p>
+            )}
+          </CardFooter>
+        </Card>
+      </form>
     </MaxWidthWrapper>
   )
 }
--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -66,5 +66,6 @@ const Message = forwardRef<HTMLDivElement, MessageProps>(
     )
   }
 )
+Message.displayName = 'Message'
 
 export default Message
